e60029c7b551c01b3748661dcc1a1c535f6b37fc
BinniZenobioCordovaLeandro <binni.2000.cordova@gmail.com>
2022-05-28 05:13:02 -0500

    new StackNavigators created, to support responsive routes

---
+++ navigation/rootNavigator.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import ModalScreen from "../screens/ModalScreen";
+import { BottomTabNavigator } from "./bottomTabNavigator";
+import TabOneScreen from "../screens/TabOneScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const StackDesktop = createNativeStackNavigator<RootStackParamList>();
+
+export function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
+        <Stack.Screen name="Modal" component={ModalScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+}
+
+export function RootNavigatorDesktop() {
+  return (
+    <StackDesktop.Navigator>
+      <StackDesktop.Screen
+        name="Root"
+        component={TabOneScreen}
+        options={{ title: "Tab One" }}
+      />
+      <StackDesktop.Screen
+        name="TabTwo"
+        component={TabTwoScreen}
+        options={{ title: "Tab Two" }}
+      />
+      <StackDesktop.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <StackDesktop.Group screenOptions={{ presentation: "modal" }}>
+        <StackDesktop.Screen name="Modal" component={ModalScreen} />
+      </StackDesktop.Group>
+    </StackDesktop.Navigator>
+  );
+}
