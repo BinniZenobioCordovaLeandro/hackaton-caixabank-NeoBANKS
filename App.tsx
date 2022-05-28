1be3d4c5e85bc469906cc36c319e222f00fc8782
BinniZenobioCordovaLeandro <binni.2000.cordova@gmail.com>
2022-05-28 03:48:00 -0500

    added support to responsive design

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
+import '@expo/match-media'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useMediaQuery } from "react-responsive";
+import { Text } from './components/Themed';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -8,15 +11,22 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const isTabletOrMobileDevice = useMediaQuery({    
+    maxDeviceWidth: 1224,
+    query: "(max-device-width: 1224px)"  
+  });
 
   if (!isLoadingComplete) {
     return null;
   } else {
-    return (
+    if (isTabletOrMobileDevice) return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+    );
+    return (
+      <Text>Navigation stack to desktop, without status bar</Text>
     );
   }
 }
