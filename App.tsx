d54c6f1b527c6bae70ebbd3cbec14dcc9fd1f8d0
BinniZenobioCordovaLeandro <binni.2000.cordova@gmail.com>
2022-05-28 04:15:10 -0500

    first prettier-format executed

--- App.tsx
+++ App.tsx
@@ -1,32 +1,31 @@
-import '@expo/match-media'
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "@expo/match-media";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useMediaQuery } from "react-responsive";
-import { Text } from './components/Themed';
+import { Text } from "./components/Themed";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const isTabletOrMobileDevice = useMediaQuery({    
+  const isTabletOrMobileDevice = useMediaQuery({
     maxDeviceWidth: 1224,
-    query: "(max-device-width: 1224px)"  
+    query: "(max-device-width: 1224px)",
   });
 
   if (!isLoadingComplete) {
     return null;
   } else {
-    if (isTabletOrMobileDevice) return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-    return (
-      <Text>Navigation stack to desktop, without status bar</Text>
-    );
+    if (isTabletOrMobileDevice)
+      return (
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      );
+    return <Text>Navigation stack to desktop, without status bar</Text>;
   }
 }
